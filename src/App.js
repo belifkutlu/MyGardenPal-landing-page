8e412823bdd801bd6f615d1a30a69c951a98bc55
belifkutlu <elifkutlu41@gmail.com>
2022-06-11 02:08:02 +0300

    add header

--- src/App.js
+++ src/App.js
@@ -1,36 +1,91 @@
 import "./App.css";
-import image from "./assets/images/logo.png";
+import image from "./assets/images/logo.svg";
+import save from "./assets/images/save.svg";
+import profil from "./assets/images/profil.svg";
+import women from "./assets/images/women.png";
+import play from "./assets/images/play.svg";
+import vase from "./assets/images/vase.jpg";
+import icon1 from "./assets/images/icon1.svg";
+import icon2 from "./assets/images/icon2.svg";
+import icon3 from "./assets/images/icon3.svg";
+import icon4 from "./assets/images/icon4.svg";
+import icon5 from "./assets/images/icon5.svg";
+import icon6 from "./assets/images/icon6.svg";
+import banner from "./assets/images/banner.png";
 
 function App() {
   return (
-    <div className="Container">
+    <div className="container">
       <header className="header">
+        <img src={image} alt="logo" />
         <navbar className="header-left">
-          <img src="{image}" alt="logo" />
-          <a href="/"></a>
-          <a href="/"></a>
-          <a href="/"></a>
-          <a href="/"></a>
+          <a href="/">Home</a>
+          <a href="/">Why Garden?</a>
+          <a href="/">Plant Guide</a>
+          <a href="/">Donate</a>
         </navbar>
         <div className="header-right">
-          <button></button>
-          <input type="search" name="" id="" />
+          <input placeholder="Search here" type="search" name="" id="" />
+          <button>
+            <img src={profil} alt="profil" />
+            Login
+          </button>
         </div>
       </header>
       <section className="section-1">
         <div className="hero-image">
-          <div className="card-1"></div>
-          <div className="card-2"></div>
+          <div className="card-1">
+            <img src={women} alt="" />
+            <button>
+              <img src={play} alt="" />
+              <span>How we works</span> <span>Watch our work process</span>
+            </button>
+          </div>
+          <div className="card-2">
+            <img src={vase} alt="" />
+            <button>Donate now</button>
+          </div>
         </div>
         <div className="content-texts-wrapper">
           <div className="content">
-            <h1></h1>
-            <p></p>
-            <button></button>
+            <h1>We bring nature to your sweet home</h1>
+            <p>
+              MyGardenPal is the best in class gardening service for your garden
+              need. Discounts available for early subscribers.
+            </p>
+            <button>
+              <img src={save} alt="" />
+              Become a volunteer
+            </button>
           </div>
           <div className="provides">
-            <h3></h3>
-            <div className="icons"></div>
+            <h3>We provides</h3>
+            <div className="icons">
+              <div>
+                {" "}
+                <img src={icon1} alt="" /> <span> Soil Test</span>{" "}
+              </div>
+              <div>
+                {" "}
+                <img src={icon2} alt="" /> <span> Bug Killing</span>{" "}
+              </div>
+              <div>
+                {" "}
+                <img src={icon3} alt="" /> <span> Pots & Planters</span>{" "}
+              </div>
+              <div>
+                {" "}
+                <img src={icon4} alt="" /> <span> Lawn Mowing</span>{" "}
+              </div>
+              <div>
+                {" "}
+                <img src={icon5} alt="" /> <span>Fencing</span>{" "}
+              </div>
+              <div>
+                {" "}
+                <img src={icon6} alt="" /> <span>Decoration</span>{" "}
+              </div>
+            </div>
           </div>
         </div>
       </section>
